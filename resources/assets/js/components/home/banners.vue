09ce3e7603757b11211772f6919efa6a2b4c393b
rsbertoa90 <rsbertoa90@gmail.com>
2018-08-25 21:05:48 -0300

    home images

--- resources/assets/js/components/home/banners.vue
+++ resources/assets/js/components/home/banners.vue
@@ -2,25 +2,25 @@
     <div >
         <div class="row">
             <div class="col-12 col-lg-8 p-1">
-                <a href="/cotizer" class="w-100 d-block">
-                    <img src="/storage/images/app/no-image.png" alt="cotizador">
+                <a href="/cotizador" class="w-100 d-block">
+                    <img src="/storage/images/app/cotizador.gif" alt="cotizador">
                 </a>
             </div>
             <div class="col-12 col-lg-4 row p-1">
                 <div class="small-banner col-12 p-0 p-lg-2 ml-lg-4">
                     <a href="/cotizer" class="w-100 d-block">
-                        <img src="/storage/images/app/no-image.png" alt="cotizador">
+                        <img src="/storage/images/app/regalos-empresariales.png" alt="regalos-empresariales">
                     </a>
                 </div>
-                <div class="small-banner col-12 p-0 p-lg-2 ml-lg-4">
-                     <a href="/cotizer" class="w-100 d-block">
-                        <img src="/storage/images/app/no-image.png" alt="cotizador">
+                <div class="small-banner mt-lg-1 col-12 p-0 p-lg-2 ml-lg-4">
+                     <a href="/sucursales" class="w-100 d-block">
+                        <img src="/storage/images/app/sucursales.png" alt="sucursales">
                     </a>
                 </div>
                 
-                <div class="small-banner col-12 p-0 p-lg-2 ml-lg-4">
-                     <a href="/cotizer" class="w-100 d-block">
-                        <img src="/storage/images/app/no-image.png" alt="cotizador">
+                <div class="small-banner mt-lg-1 col-12 p-0 p-lg-2 ml-lg-4">
+                     <a href="/franquicia" class="w-100 d-block">
+                        <img src="/storage/images/app/franquicia.png" alt="franquicia">
                     </a>
                 </div>
             </div>
@@ -44,6 +44,7 @@ export default {
     @media(min-width: 990px){
         .small-banner
         {
+            max-width: 89%;
             padding : 5px 15px ;
         }
     }

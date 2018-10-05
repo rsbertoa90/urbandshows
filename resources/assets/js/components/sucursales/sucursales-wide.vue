00f7d0d177f8c7cd67b9b8d6a9c11c0f638af5dd
rsbertoa90 <rsbertoa90@gmail.com>
2018-10-05 13:56:58 -0300

    iframe titles

--- resources/assets/js/components/sucursales/sucursales-wide.vue
+++ resources/assets/js/components/sucursales/sucursales-wide.vue
@@ -4,7 +4,7 @@
             <div class="col-12 row mt-3 mb-4">
                 <h2 class="col-12 mb-3">Mates Fabi Pasteur</h2>
                 <div class="col-6">
-                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.998628875948!2d-58.40179358477038!3d-34.60419618045944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf355d9f72cc6a37!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754534548" 
+                   <iframe  title="Mates Fabi Pasteur" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.998628875948!2d-58.40179358477038!3d-34.60419618045944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf355d9f72cc6a37!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754534548" 
                         width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <div class="col-6">
@@ -21,7 +21,7 @@
                 <h2 class="col-12 mb-3">Mates Fabi Castelli</h2>
                 <div class="col-6">
 
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.9161964748396!2d-58.406728984770254!3d-34.60628058045892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8c4efba903228b26!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754738056" 
+                    <iframe  title="Mates Fabi Castelli" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.9161964748396!2d-58.406728984770254!3d-34.60628058045892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8c4efba903228b26!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754738056" 
                 width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
 
                 </div>
@@ -39,7 +39,7 @@
                 <div class="col-6">
                       
                       
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.925118556445!2d-58.40105768477033!3d-34.60605498045881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4299f41bc872ccdc!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754660710" 
+                <iframe  title="Mates Fabi Sarmiento" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.925118556445!2d-58.40105768477033!3d-34.60605498045881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4299f41bc872ccdc!2sMates+Fabi!5e0!3m2!1ses!2sar!4v1538754660710" 
                         width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
                 
                 </div>

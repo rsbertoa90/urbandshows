efd10ca52644a0d9b63e764d3818a129dac05562
rsbertoa90 <rsbertoa90@gmail.com>
2018-10-08 14:56:45 -0300

    name in orders

--- resources/assets/js/components/admin/order.vue
+++ resources/assets/js/components/admin/order.vue
@@ -48,7 +48,7 @@
         </div>
         <div>
                 <ul>
-                    <li v-if="order.name">Nombre del cliente: {{order.name}} </li>
+                    <li v-if="order.name">Nombre del cliente: {{order.client}} </li>
                     <li v-if="order.phone">Telefono: {{order.phone}} </li>
                     <li v-if="order.email">Email: {{order.email}} </li>
                     <li v-if="order.message">Mensaje adjunto: {{order.message}} </li>
